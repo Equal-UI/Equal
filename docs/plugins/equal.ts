9103c7cc9a69cf8417d48f720efda7335271d7fb
Yan Savinov <yan.savinov.firefly@gmail.com>
2019-10-16 10:42:32 +0300

    fxfxf

--- docs/plugins/equal.ts
+++ docs/plugins/equal.ts
@@ -1,4 +1,27 @@
 import Vue from 'vue'
 import Equal from '../../src'
 
-Vue.use(Equal)
+// Vue.use(Equal)
+// const components = {
+//   Alert,
+//   Button,
+//   Checkbox,
+//   ...Dropdown,
+//   Icon,
+//   Input,
+//   Message,
+//   Modal,
+//   Notification,
+//   NumberInput,
+//   Progressbar,
+//   Radio,
+//   ...Select,
+//   Slider,
+//   Switch,
+//   Tag,
+//   Textarea,
+//   Tooltip
+// }
+
+Vue.component('it-alert', Equal.Alert)
+Vue.component('it-button', Equal.Button)
