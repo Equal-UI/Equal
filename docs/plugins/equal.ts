806bb29f32f0674797056501e701f7223b7d9501
Yan Savinov <yan.savinov.firefly@gmail.com>
2019-10-16 10:58:34 +0300

    fxfxf

--- docs/plugins/equal.ts
+++ docs/plugins/equal.ts
@@ -25,3 +25,23 @@ import Equal from '../../src'
 
 Vue.component('it-alert', Equal.Alert)
 Vue.component('it-button', Equal.Button)
+Vue.component('it-checkbox', Equal.Checkbox)
+Vue.component('it-dropdown', Equal.Dropdown)
+Vue.component('it-dropdown-menu', Equal.DropdownMenu)
+Vue.component('it-dropdown-item', Equal.DropdownItem)
+Vue.component('it-icon', Equal.Icon)
+Vue.component('it-input', Equal.Input)
+Vue.component('it-number-input', Equal.NumberInput)
+Vue.component('it-progressbar', Equal.Progressbar)
+Vue.component('it-radio', Equal.Radio)
+Vue.component('it-select', Equal.Select)
+Vue.component('it-select-option', Equal.SelectOption)
+Vue.component('it-slider', Equal.Slider)
+Vue.component('it-switch', Equal.Switch)
+Vue.component('it-tag', Equal.Tag)
+Vue.component('it-textarea', Equal.Textarea)
+Vue.component('it-tooltip', Equal.Tooltip)
+
+Vue.prototype.$Message = Equal.Message
+Vue.prototype.$Modal = Equal.Modal
+Vue.prototype.$Notification = Equal.Notification
