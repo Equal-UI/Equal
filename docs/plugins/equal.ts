74bd540b43352e1b0d61e2dd7068784c1f528d00
Yan Savinov <yan.savinov.firefly@gmail.com>
2019-10-16 13:48:40 +0300

    fxfxf

--- docs/plugins/equal.ts
+++ docs/plugins/equal.ts
@@ -1,28 +1,6 @@
 import Vue from 'vue'
 import Equal from '../../src'
 
-// Vue.use(Equal)
-// const components = {
-//   Alert,
-//   Button,
-//   Checkbox,
-//   ...Dropdown,
-//   Icon,
-//   Input,
-//   Message,
-//   Modal,
-//   Notification,
-//   NumberInput,
-//   Progressbar,
-//   Radio,
-//   ...Select,
-//   Slider,
-//   Switch,
-//   Tag,
-//   Textarea,
-//   Tooltip
-// }
-
 Vue.component('it-alert', Equal.Alert)
 Vue.component('it-button', Equal.Button)
 Vue.component('it-checkbox', Equal.Checkbox)
@@ -31,11 +9,8 @@ Vue.component('it-dropdown-menu', Equal.DropdownMenu)
 Vue.component('it-dropdown-item', Equal.DropdownItem)
 Vue.component('it-icon', Equal.Icon)
 Vue.component('it-input', Equal.Input)
-// Vue.component('it-message', Equal.Message)
-// Vue.component('it-modal', Equal.Modal)
-// Vue.component('it-notification', Equal.Notification)
 Vue.component('it-number-input', Equal.NumberInput)
-// Vue.component('it-progressbar', Equal.Progressbar)
+Vue.component('it-progressbar', Equal.Progressbar)
 Vue.component('it-radio', Equal.Radio)
 Vue.component('it-select', Equal.Select)
 Vue.component('it-select-option', Equal.SelectOption)
