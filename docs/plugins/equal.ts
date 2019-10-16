392a54e1e1f00cfca4fc2ddb5a719633551b63b5
Yan Savinov <yan.savinov.firefly@gmail.com>
2019-10-16 11:21:51 +0300

    fxfxf

--- docs/plugins/equal.ts
+++ docs/plugins/equal.ts
@@ -34,17 +34,17 @@ Vue.component('it-input', Equal.Input)
 // Vue.component('it-message', Equal.Message)
 // Vue.component('it-modal', Equal.Modal)
 // Vue.component('it-notification', Equal.Notification)
-// Vue.component('it-number-input', Equal.NumberInput)
-// Vue.component('it-progressbar', Equal.Progressbar)
-// Vue.component('it-radio', Equal.Radio)
+Vue.component('it-number-input', Equal.NumberInput)
+Vue.component('it-progressbar', Equal.Progressbar)
+Vue.component('it-radio', Equal.Radio)
 // Vue.component('it-select', Equal.Select)
 // Vue.component('it-select-option', Equal.SelectOption)
-// Vue.component('it-slider', Equal.Slider)
-// Vue.component('it-switch', Equal.Switch)
-// Vue.component('it-tag', Equal.Tag)
-// Vue.component('it-textarea', Equal.Textarea)
-// Vue.component('it-tooltip', Equal.Tooltip)
+Vue.component('it-slider', Equal.Slider)
+Vue.component('it-switch', Equal.Switch)
+Vue.component('it-tag', Equal.Tag)
+Vue.component('it-textarea', Equal.Textarea)
+Vue.component('it-tooltip', Equal.Tooltip)
 
-// Vue.prototype.$Message = Equal.Message
-// Vue.prototype.$Modal = Equal.Modal
-// Vue.prototype.$Notification = Equal.Notification
+Vue.prototype.$Message = Equal.Message
+Vue.prototype.$Modal = Equal.Modal
+Vue.prototype.$Notification = Equal.Notification
