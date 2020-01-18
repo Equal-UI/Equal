f5b12bbeb235dd0061fee5f5918b38cab2dee05f
Yan Savinov <yan.savinov.firefly@gmail.com>
2020-01-18 08:32:00 +0300

    nuxt file fix

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -14,7 +14,7 @@ export default {
     title: 'Equal components' || process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width:device-width, initial-scale:1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
