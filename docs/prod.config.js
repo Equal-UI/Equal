29d018c9c47bcff07ad4ba6c2d9a7c0d0de780f5
yan <piraten.pad@gmail.com>
2021-02-13 11:39:03 +0300

    docs

--- docs/prod.config.js
+++ docs/prod.config.js
@@ -3,11 +3,12 @@ import vue from '@vitejs/plugin-vue'
 console.log(import.meta)
 module.exports = {
   optimizeDeps: {
-    exclude: 'prismjs'
+    exclude: ['prismjs', 'body-scroll-lock']
   },
   plugins: [vue()],
   alias: {
     '/@equal': path.resolve(__dirname, './src/equal'),
+    '@': path.resolve(__dirname, './src/equal'),
   },
   base: '/Equal/',
   assetsDir: '',
