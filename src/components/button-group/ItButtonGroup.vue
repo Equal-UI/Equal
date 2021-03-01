9fec9c70765afa29a40128a1fe81832bb7e25744
Egor Lazuka (Home) <lazuka_99@mail.ru>
2021-03-01 03:20:09 +0300

    REFACTOR: ItButtonGroup component

--- src/components/button-group/ItButtonGroup.vue
+++ src/components/button-group/ItButtonGroup.vue
@@ -1,6 +1,11 @@
 <template>
-  <div :class="`it-button-group${vertical ? '--vertical' : ''}`">
-    <slot></slot>
+  <div
+    class="it-button-group"
+    :class="
+      vertical ? 'it-button-group--vertical' : 'it-button-group--horizontal'
+    "
+  >
+    <slot />
   </div>
 </template>
 
