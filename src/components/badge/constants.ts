6ab3f4835d0926bc36035b9e3df6ceed1e1026a8
Happy-Child <40471724+Happy-Child@users.noreply.github.com>
2021-03-02 14:59:44 +0300

    Refactor ItBadge component (#210)
    * FIX: responsive scroll modal
    
    * FIX: refactor ItModal, add dependencies to docs dir
    
    * PR fixes
    
    * REFACTOR: ItBadge component
    
    Co-authored-by: yan <piraten.pad@gmail.com>

---
+++ src/components/badge/constants.ts
@@ -0,0 +1,15 @@
+import { Colors, Positions } from '@/models/enums'
+
+export const ALLOWED_POSITIONS = [
+  Positions.TL,
+  Positions.TR,
+  Positions.BL,
+  Positions.BR,
+]
+
+export const ALLOWED_COLORS = [
+  Colors.PRIMARY,
+  Colors.SUCCESS,
+  Colors.DANGER,
+  Colors.WARNING,
+]
