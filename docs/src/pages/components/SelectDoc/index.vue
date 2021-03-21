617c3ba5ef9bb9a096b996a46066370d4bd909f7
Egor Lazuka (Home) <lazuka_99@mail.ru>
2021-03-21 21:41:33 +0300

    REFACTOR: ItSelect docs

---
+++ docs/src/pages/components/SelectDoc/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Select</h1>
+    <SectionDemo />
+    <SectionExamples />
+    <props-table tag-name="it-select" :data-sheet="dataSheet" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import SectionDemo from './SectionDemo/index.vue'
+import SectionExamples from './SectionExamples/index.vue'
+import { DATA_SHEET_PROPS } from './constants'
+
+export default defineComponent({
+  components: {
+    SectionDemo,
+    SectionExamples,
+  },
+  setup() {
+    return {
+      dataSheet: DATA_SHEET_PROPS,
+    }
+  },
+})
+</script>
