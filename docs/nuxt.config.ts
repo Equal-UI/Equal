9b8abdee38b592957555d56e61342e483a384e58
Yan Savinov <savinov@lanit.ru>
2023-01-16 00:01:23 +0700

    build fix

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  baseURL: '/Equal/',
+  app: {
+    baseURL: '/Equal/',
+  },
   target: 'static',
   router: {
     base: '/Equal/',
