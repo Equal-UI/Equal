04990dc6934a94fa3a561cfe3a1f66a53de12d62
Yan Savinov <savinov@lanit.ru>
2023-01-15 21:21:58 +0700

    build fix

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -9,8 +9,10 @@ export default defineNuxtConfig({
   alias: {
     '@': '/../src',
     '@floating-ui': './node_modules/@floating-ui',
+    '@vueuse/integrations/useFocusTrap':
+      './node_modules/@vueuse/integrations/useFocusTrap',
     'body-scroll-lock': './node_modules/body-scroll-lock',
-    'colord': './node_modules/colord/index.js',
+    colord: './node_modules/colord/index.js',
   },
   postcss: {
     plugins: {
