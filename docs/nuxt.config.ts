97c18dfc396494c2d290ba526b40e8b7704366a3
Yan Savinov <savinov@lanit.ru>
2023-01-15 19:58:57 +0700

    nuxt build fix

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,6 +1,3 @@
-import { defineNuxtConfig } from 'nuxt'
-import { resolve } from 'path'
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   baseURL: '/Equal/',
