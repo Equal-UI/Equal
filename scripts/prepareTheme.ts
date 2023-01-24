075d7c80234d3f8cedcc52a94560d5d7ae697606
Yan Savinov <savinov@lanit.ru>
2023-01-24 22:12:59 +0700

    build fix

--- scripts/prepareTheme.ts
+++ scripts/prepareTheme.ts
@@ -1,6 +1,6 @@
-import full from '../src/theme/full'
-import light from '../src/theme/light'
-import dark from '../src/theme/dark'
+// import full from '../src/theme/full'
+// import light from '../src/theme/light'
+// import dark from '../src/theme/dark'
 import fs from 'fs'
 
 fs.mkdirSync('./dist/theme', { recursive: true })
@@ -11,6 +11,6 @@ fs.copyFileSync('./src/theme/light.ts', './dist/theme/light.js')
 fs.copyFileSync('./src/theme/dark.ts', './dist/theme/dark.ts')
 fs.copyFileSync('./src/theme/dark.ts', './dist/theme/dark.js')
 
-fs.writeFileSync('./dist/theme/full.json', JSON.stringify(full, null, 2))
-fs.writeFileSync('./dist/theme/light.json', JSON.stringify(light, null, 2))
-fs.writeFileSync('./dist/theme/dark.json', JSON.stringify(dark, null, 2))
+// fs.writeFileSync('./dist/theme/full.json', JSON.stringify(full, null, 2))
+// fs.writeFileSync('./dist/theme/light.json', JSON.stringify(light, null, 2))
+// fs.writeFileSync('./dist/theme/dark.json', JSON.stringify(dark, null, 2))
